18bfd1dba229e917a5a1807d827517b7d98deedf
kpreddy619 <kpreddy619@gmail.com>
2017-04-30 00:42:42 +0530

    Update private_sender.py

--- private_server/private_sender.py
+++ private_server/private_sender.py
@@ -1,12 +1,13 @@
 import socket               # Import socket module
 
 s = socket.socket()         # Create a socket object
-host = socket.gethostname() # Get local machine name
+#host = socket.gethostname() # Get local machine name
+host = '192.168.0.15'
 port = 12346                 # Reserve a port for your service.
 
 def send_file_to_public(file_name):
     s.connect((host, port))
-    s.send("Hello server!")
+    s.send("asdf"+"--->put")
     f = open(file_name,'rb')
     print 'Sending...'
     l = f.read(1024)
@@ -14,8 +15,14 @@ def send_file_to_public(file_name):
         print 'Sending...'
         s.send(l)
         l = f.read(1024)
+    # "data------------>key"
     f.close()
     print "Done Sending"
+    #print s.recv(1024)
     s.shutdown(socket.SHUT_WR)
     print s.recv(1024)
     s.close                     # Close the socket when done
+
+send_file_to_public('test')
+
+
